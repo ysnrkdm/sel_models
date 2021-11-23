8bf3bde558546bc814baf39acc88622a3fd65134
Ysnr Kdm <ysnr.kdm@gmail.com>
2021-11-23 19:04:26 +0900

    add FixedLengthTokensDataset to get_dataset

--- src/sel_models/dataset/dataset_factory.py
+++ src/sel_models/dataset/dataset_factory.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict
 
 from .dataset import Dataset
+from .fixed_length_tokens_dataset import FixedLengthTokensDataset
 from .tokens_dataset import TokensDataset
 
 
@@ -8,6 +9,7 @@ def get_dataset(base_path: str, metadata: Dict[str, Any]) -> Dataset:
     ds_type = metadata['dataset_type']
     if ds_type == 'TokensDataset':
         return TokensDataset(base_path)
+    elif ds_type == 'FixedLengthTokensDataset':
+        return FixedLengthTokensDataset(base_path)
 
     return Dataset(base_path)
-
